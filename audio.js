e01e4ac011c6497acff758d90554da025236703f
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-03-12 17:26:51 -0500

    autoLoad audio files, demo

--- audio.js
+++ audio.js
@@ -16,53 +16,23 @@ function init(){
         }
 }
 
-//// audio file buffer
-//var simonSound1 = null;
-//
-//function loadSimonSound(url){
-//    var request = new XMLHttpRequest();
-//    request.open('GET', url, true);
-//    request.responseType = 'arraybuffer';
-//    
-//    request.onload = function(){
-//        context.decodeAudioData(request.response, function(buffer){
-//            simonSound1 = buffer;
-//            // playSound(simonSound1);
-//        }, onerror);
-//    }
-//    request.send();
-//}
-//
-//loadSimonSound('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'); 
-//
-//// playback sound function
-//
-//function playSound(buffer){
-//    var source = context.createBufferSource();
-//    source.buffer = buffer;
-//    source.connect(context.destination);
-//    source.start(0);
-//}
-
-
-
 // load multiple audion files 
 
 var audioSources = {
     simon1: {
-        buffer: "null", 
+        playback: "null", 
         webLink : 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'
     },
     simon2: {
-        buffer: "null", 
+        playback: "null", 
         webLink : 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'
     },
     simon3: {
-        buffer: "null", 
+        playback: "null", 
         webLink : 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'
     },
     simon4: {
-        buffer: "null", 
+        playback: "null", 
         webLink : 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
     }
 }
@@ -70,42 +40,66 @@ var audioSources = {
 function AudioLoadBuffer(source){
     this.source = source;
     
-    for(var file in source){
-        httpReq(source[file].webLink, source[file].buffer);
+    this.loadData = function(){
+        for(var file in source){
+           httpReq(file);
+        }
     }
     
-    function httpReq(url, buffer){
+    function httpReq(file){
         //console.log(url);
         var request = new XMLHttpRequest();
-        request.open('GET', url, true);
+        request.open('GET', source[file].webLink, true);
         request.responseType = 'arraybuffer'; 
         request.onload = function(){
-            context.decodeAudioData(request.response, function(res){
-                buffer = res;
-            })
+             context.decodeAudioData(request.response, function(res){
+                audioSources[file].playback = res;
+            });
+            
         }
-        
-        request.send();
-        
+        request.send();  
     }
-    
-//    function decodeAudio(buffer, response){
-//        context.decodeAudioData(response, function(buffer){
-//            buffer = buffer;
-//        }, onerror);
-//    }
-//    
-    return source;
     
 }
 
-var audioFiles = new AudioLoadBuffer(audioSources);
+var newAudio = new AudioLoadBuffer(audioSources);
+newAudio.loadData();
 
-function playSound(buffer){
+
+function playBack(buffer){
     var source = context.createBufferSource();
     source.buffer = buffer;
     source.connect(context.destination);
     source.start(0);
 }
+
+
+
+
+// =========  interface with audio playback ==========
+
+
+function playSound(button) {
+    switch (button) {
+        case 1 : playBack(audioSources.simon1.playback); break;
+        case 2 : playBack(audioSources.simon2.playback); break;
+        case 3 : playBack(audioSources.simon3.playback); break;
+        case 4 : playBack(audioSources.simon4.playback); break;
+        default : console.log("bad input");
+    }
+}
+
+var buttonOne = document.getElementById("button1").addEventListener('click', function(){
+    playSound(1);
+});
+var buttonTwo = document.getElementById("button2").addEventListener('click', function(){
+    playSound(2);
+});
+var buttonThree = document.getElementById("button3").addEventListener('click', function(){
+    playSound(3);
+});
+var buttonFour = document.getElementById("button4").addEventListener('click', function(){
+    playSound(4);
+});
 
 
